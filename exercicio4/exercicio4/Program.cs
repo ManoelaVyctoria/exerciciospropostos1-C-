f8437d50ab7971df26025ec7741fe15c6eee3647
MANOELA VYCTORIA TAVARES DOS SANTOS <74735404+MANOELAVYCTORIA@users.noreply.github.com>
2021-02-04 15:47:16 -0300

    exercicio4

---
+++ exercicio4/exercicio4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+namespace exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = int.Parse(Console.ReadLine());
+            int horas = int.Parse(Console.ReadLine());
+            double valorhora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salario;
+
+            salario = valorhora * horas;
+
+            Console.WriteLine("NUMBER = " + numero );
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture ));
+        }
+    }
+}
