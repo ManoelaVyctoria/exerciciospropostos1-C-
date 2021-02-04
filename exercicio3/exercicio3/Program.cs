a3a164c1bc9aa6b9bdda1cc4f980d334957150a2
MANOELA VYCTORIA TAVARES DOS SANTOS <74735404+MANOELAVYCTORIA@users.noreply.github.com>
2021-02-04 15:46:48 -0300

    exercicio3

---
+++ exercicio3/exercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            int dif;
+
+            dif = a * b - c * d;
+
+            Console.WriteLine(dif);
+        }
+    }
+}
