317fe3c076596bec4aa4ccf2b7b68b9c684e53fc
MANOELA VYCTORIA TAVARES DOS SANTOS <74735404+MANOELAVYCTORIA@users.noreply.github.com>
2021-02-04 15:47:26 -0300

    exercicio5

---
+++ exercicio5/exercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+namespace exercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int codigo1 = int.Parse(Console.ReadLine());
+            int qtd1 = int.Parse(Console.ReadLine());
+            double valor1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            int codigo2 = int.Parse(Console.ReadLine());
+            int qtd2 = int.Parse(Console.ReadLine());
+            double valor2 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            double total;
+
+            total = valor1 * qtd1 + valor2 * qtd2;
+
+            Console.WriteLine("VALOR A PAGAR: R$"+ total.ToString("F2", CultureInfo.InvariantCulture));
+
+
+        }
+    }
+}
