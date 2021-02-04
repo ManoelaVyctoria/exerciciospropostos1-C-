b2257a37c2059be01a2c9127588bb9260b6057ac
MANOELA VYCTORIA TAVARES DOS SANTOS <74735404+MANOELAVYCTORIA@users.noreply.github.com>
2021-02-04 15:46:37 -0300

    exercicio2

---
+++ exercicio2/exercicio2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+namespace exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r,a,pi = 3.14159;
+
+            r = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            a = pi * r * r;
+
+            Console.WriteLine("A= " + a.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
